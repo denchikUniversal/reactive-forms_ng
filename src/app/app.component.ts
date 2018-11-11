8a132a0b520dac8e6c8bf8489f842584f70dac40
denchikUniversal <tkachenkodenys94@gmail.com>
2018-11-11 21:51:49 +0200

    patchvalue method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,18 @@ export class AppComponent implements OnInit {
     });
   }
 
+  onLoadData(): void {
+    this.employeeForm.patchValue({
+      fullName: 'TestName',
+      email: 'testName@gmail.com',
+      skills: {
+        skillName: 'JavaScript',
+        experienceInYears: 5,
+        proficiency: 'advanced'
+      }
+    })
+  }
+
   onSubmit(): void {
     console.log(this.employeeForm.touched);
     console.log(this.employeeForm.value);
